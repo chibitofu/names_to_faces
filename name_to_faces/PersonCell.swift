fa9679d85c7f272356ec75503ef30f50830ebd56
chibitofu <erinmoony@gmail.com>
2017-10-19 19:53:32 -0700

    Basic setup of UICollectionView.

---
+++ name_to_faces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  name_to_faces
+//
+//  Created by Erin Moon on 10/19/17.
+//  Copyright © 2017 Erin Moon. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
