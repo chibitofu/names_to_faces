5bf0daf2ab57ce4960087afaa329024224aa3725
chibitofu <erinmoony@gmail.com>
2017-10-20 19:59:54 -0700

    Updated the view.

--- name_to_faces/ViewController.swift
+++ name_to_faces/ViewController.swift
@@ -8,7 +8,14 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @objc func addNewPerson() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 10
@@ -22,6 +29,7 @@ class ViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
         // Do any additional setup after loading the view, typically from a nib.
     }
 
